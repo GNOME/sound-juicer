2b6d296a7c329a66cd348b8fd3f6c4f6da9438fa
Bastien Nocera <hadess@hadess.net>
2003-09-16 17:58:28 +0000

    Fix the test so that it works and compiles.
    2003-09-16  Bastien Nocera  <hadess@hadess.net>
    
    	* tests/mb-test.c: (metadata_cb), (main):
    	Fix the test so that it works and compiles.

--- tests/mb-test.c
+++ tests/mb-test.c
@@ -1,17 +1,18 @@
 #include <glib.h>
+#include <stdlib.h>
 #include "sj-structures.h"
 #include "sj-metadata.h"
 #include "sj-metadata-musicbrainz.h"
 
-int main (int argc, char** argv)
+static void
+metadata_cb (SjMetadata *metadata, GList *albums, GError *error)
 {
-  GList *albums = NULL;
-  SjMetadata *metadata;
-
-  g_type_init ();
-  metadata = (SjMetadata*)sj_metadata_musicbrainz_new ();
-  sj_metadata_set_cdrom (metadata, "/dev/cdroms/cdrom0");
-  //albums = sj_metadata_list_albums (metadata, NULL);
+  if (error != NULL) {
+    g_print ("Error: %s\n", error->message);
+    g_error_free (error);
+    g_object_unref (metadata);
+    exit (1);
+  }
 
   while (albums) {
     AlbumDetails *album;
@@ -24,6 +25,34 @@ int main (int argc, char** argv)
     }
     albums = g_list_next (albums);
   }
+
   g_object_unref (metadata);
+  exit (0);
+}
+
+int main (int argc, char** argv)
+{
+  SjMetadata *metadata;
+  GMainLoop *loop;
+
+  g_type_init ();
+  metadata = (SjMetadata*)sj_metadata_musicbrainz_new ();
+
+  if (argc == 2) {
+    sj_metadata_set_cdrom (metadata, argv[1]);
+  } else if (argc == 1) {
+    sj_metadata_set_cdrom (metadata, "/dev/cdroms/cdrom0");
+  } else {
+    g_print ("Usage: %s [CD device]\n", argv[0]);
+    exit (1);
+  }
+
+  g_signal_connect (G_OBJECT (metadata), "metadata",
+		    G_CALLBACK (metadata_cb), NULL);
+  sj_metadata_list_albums (metadata, NULL);
+
+  loop = g_main_loop_new (NULL, FALSE);
+  g_main_loop_run (loop);
+
   return 0;
 }
