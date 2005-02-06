f859ade3181c99a611274ac46b278ecd4404e9bb
Ross Burton <rburton@src.gnome.org>
2005-02-06 17:26:32 +0000

    Fix statement order

--- src/sj-main.c
+++ src/sj-main.c
@@ -565,8 +565,8 @@ static void reread_cd (gboolean ignore_no_media)
  */
 void device_changed_cb (GConfClient *client, guint cnxn_id, GConfEntry *entry, gpointer user_data)
 {
-  g_assert (strcmp (entry->key, GCONF_DEVICE) == 0);
   gboolean ignore_no_media = GPOINTER_TO_INT (user_data);
+  g_assert (strcmp (entry->key, GCONF_DEVICE) == 0);
 
   if (entry->value == NULL
 		  || !cd_drive_exists (gconf_value_get_string (entry->value))) {
