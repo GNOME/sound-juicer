551b00395eb1049629d15a67b8cc920736b5786c
Christian Kirbach <Christian.Kirbach@googlemail.com>
2007-03-20 09:38:22 +0000

    Initialise threading system before calling gst_init_get_option_group()
    2007-03-15  Christian Kirbach  <Christian.Kirbach@googlemail.com>
    
    	* src/sj-main.c:
    	Initialise threading system before calling gst_init_get_option_group()
    	(#418717).
    
    svn path=/trunk/; revision=1731

--- src/sj-main.c
+++ src/sj-main.c
@@ -1311,6 +1311,8 @@ int main (int argc, char **argv)
     { NULL }
   };
 
+  if (!g_thread_supported ()) g_thread_init (NULL);
+
   bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
