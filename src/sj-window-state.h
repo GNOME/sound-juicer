87f4f3a798d8b1a7ebdce144bdd571d6d061d57b
Phillip Wood <phillip.wood@dunelm.org.uk>
2014-08-15 10:25:28 +0100

    Fix license of sj-window-state.[ch]
    These should have been licensed under GPL2 or later but I originally
    copied the license header for LGPL2 by a mistake.

--- src/sj-window-state.h
+++ src/sj-window-state.h
@@ -1,20 +1,20 @@
 /*
- * sj-window-state.h
  * Copyright (C) 2014 Phillip Wood <phillip.wood@dunelm.org.uk>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * Sound Juicer - sj-window-state.h
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SJ_WINDOW_STATE_H
